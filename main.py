ab8b8fd9b0ffc220ffd2070f4cbe32d01ff27aea
pettyblue <pettyblue@proton.me>
2023-04-22 18:24:48 +0900

    create board

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Constant values
 WINDOW_SIZE = (173, 200)
@@ -27,6 +28,18 @@ window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption(WINDOW_TITLE)
 pygame.display.set_icon(bomb_img)
 
+
+class Cell():
+    is_bomb = False
+    x = 0
+    y = 0
+
+    def __init__(self, pos):
+        self.pos = pos
+        # "Normal" | "Flagged" | "Blank" | "Warn1" to "Warn8" | "Exploded"
+        self.flag = "Normal"
+
+
 def show_timer():
     # Show clock image
     image_position = (10, 173)
@@ -38,7 +51,8 @@ def show_timer():
     secs = int(ticks_now / 1000 % 60)
 
     # Fill with zero
-    timer_mins = ""; timer_secs = ""
+    timer_mins = ""
+    timer_secs = ""
     if mins < 10:
         timer_mins = "0" + str(mins)
     else:
@@ -50,6 +64,34 @@ def show_timer():
     timer_time = font.render(timer_mins + ":" + timer_secs, True, black)
     text_position = (30, 173)
     window.blit(timer_time, text_position)
+
+
+def init_cells():
+    for x in range(9):
+        for y in range(9):
+            cell_pos = (x*17 + 10, y*17 + 10)
+            cell_pos_rect = pygame.Rect(cell_pos, (17, 17))
+            cell = Cell(cell_pos_rect)
+            cell.x = x
+            cell.y = y
+            cells.append(cell)
+
+
+def pick_boms(num_boms):
+    global num_boms_now
+    while num_boms_now < num_boms:
+        cells_len = len(cells)
+        random_index = random.randrange(cells_len)
+        if not cells[random_index].is_bomb:
+            cells[random_index].is_bomb = True
+            num_boms_now += 1
+
+
+cells = []
+num_boms_now = 0
+
+init_cells()
+pick_boms(10)
 
 # Main loop
 running = True
